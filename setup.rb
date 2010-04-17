45c234c6026124ba5477b3271f824fc75ba42739
Vaz <allen.mat@gmail.com>
2010-04-17 16:05:02 -0400

    Initial commit

---
+++ setup.rb
@@ -0,0 +1,58 @@
+#!/usr/bin/env ruby
+
+if `which git` and $? != 0
+  puts "You probably want to install git."
+end
+
+if `which hg` and $? != 0
+  puts "You probably want to install hg."
+end
+
+if ARGV[0] == '-f'
+  puts "Not creating backups!"
+  destructive = true
+end
+
+HOME = ENV['HOME']
+DOTFILES = File.expand_path(File.dirname(__FILE__))
+
+dotfiles = {
+  'bash' => {
+    "bashrc.#{`uname`.chomp}" => "#{HOME}/.bashrc",
+    "bash_aliases"    => "#{HOME}/.bash_aliases",
+    "bash_defs"       => "#{HOME}/.bash_defs",
+    "profile"         => "#{HOME}/.profile"
+  },
+  'git'  => {
+    "gitconfig"       => "#{HOME}/.gitconfig"
+  },
+  'hg' => {
+    "hgrc"            => "#{HOME}/.hgrc"
+  },
+  'ruby' => {
+    "autotest"        => "#{HOME}/.autotest",
+    "irbrc"           => "#{HOME}/.irbrc"
+  },
+  'vim' => {
+    "vimrc"           => "#{HOME}/.vimrc",
+    "vimhome"         => "#{HOME}/.vim"
+  }
+}
+
+dotfiles.each do |dir, files|
+  files.each do |src, dst|
+    if File.exists? dst
+      if destructive or File.symlink? dst
+        puts "delete:  #{dst}"
+        File.delete dst
+      else
+        puts "backup:  #{dst} to #{dst}.bak"
+        File.rename(dst, "#{dst}.bak")
+      end
+    end
+
+    src = "#{DOTFILES}/#{dir}/#{src}"
+    File.symlink src, dst
+    puts "symlink: #{dst} -> #{src}"
+  end
+end
