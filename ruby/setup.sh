d63912a9ad6f1938e4369ca2d38b0a6b67ae465e
Vaz Allen <vaz@tryptid.com>
2012-09-13 00:41:03 +0000

    Update ruby version

--- ruby/setup.sh
+++ ruby/setup.sh
@@ -5,7 +5,7 @@ mkdir -p $HOME/.rbenv/plugins
 git clone git://github.com/sstephenson/ruby-build.git $HOME/.rbenv/plugins/ruby-build
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
-rbenv versions | grep '1.9.3-p125' || rbenv install 1.9.3-p125
+rbenv versions | grep '1.9.3-p194' || rbenv install 1.9.3-p194
 rbenv global 1.9.3-p125
 rbenv rehash
 gem install bundler --no-ri --no-rdoc
