215173dc98448319c82f140b445bc3a32f74dc5f
Vaz Allen <vaz@tryptid.com>
2013-02-20 16:01:28 -0800

    Update vim compile script, I forget if it works.

--- vim/compile-vim.sh
+++ vim/compile-vim.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-cd /tmp
-hg clone https://vim.googlecode.com/hg/ vim
-cd vim/src
-./configure --prefix=/usr/local --with-features=huge --enable-rubyinterp --enable-pythoninterp --enable-perlinterp --enable-cscope --enable-multibyte
-make
-sudo make install
+VIM_PREFIX=/usr/local
+
+dir="/tmp/dotfiles/vim-$(date| shasum| cut -d' ' -f1)"
+mkdir -p $(dirname $dir)
+
+hg clone https://vim.googlecode.com/hg/ "$dir"
+
+cd "$dir/src"
+{
+  ./configure \
+      --prefix="$VIM_PREFIX" \
+      --with-features=huge \
+      --enable-rubyinterp \
+      --enable-pythoninterp \
+      --enable-perlinterp \
+      --enable-cscope \
+      --enable-multibyte
+} && make && sudo make install
 hash -r
 
