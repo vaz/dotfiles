a41136e086a95adb28c14737e455fbcd473e838a
Vaz Allen <vaz@tryptid.com>
2013-03-21 01:56:21 +0000

    echo

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,5 @@
 sudo apt-get install ruby rubygems
 sudo gem install rake --no-rdoc --no-ri
 sudo ln -sf /var/lib/gems/1.8/bin/rake /usr/bin/rake
+echo
+echo "ready... run 'rake' to set everything up"
