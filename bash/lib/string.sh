42480ca883fa95272ddff3f004a5dada576b117a
Vaz Allen <vaz@tryptid.com>
2013-04-01 03:56:11 +0000

    Colour stuff

--- bash/lib/string.sh
+++ bash/lib/string.sh
@@ -31,6 +31,9 @@ includes? () { quietly indexof "$@"; }
 # bold
 @s () { [ -n "$TERM" ] && echo -n $(tput bold); }
 
+# reverse
+@S () { [ -n "$TERM" ] && echo -n $(tput rev); }
+
 # underline
 @u () { [ -n "$TERM" ] && echo -n $(tput smul); }
 
@@ -40,8 +43,9 @@ includes? () { quietly indexof "$@"; }
 # reset
 @x () { [ -n "$TERM" ] && echo -n $(tput sgr0); }
 
+# TODO: use setaf/setab/etc;  see 'man terminfo'
 _colour () {
-  esc "[$1;$2m"; shift 2
+  esc "[${1}m"; shift
   (( $# )) && {
     echo -n "$@"
     @x
@@ -49,29 +53,38 @@ _colour () {
 }
 
 @ () {
-  [[ $1 = [suUx] ]] && { eval "@$1 $@"; return; }
-  local codes='krgybmcwKRGYBMCW' b=0 c="${1:0:1}" bg=0
+  [[ $1 = [sSuUx] ]] && { eval "@$1 $@"; return; }
+  local codes='krgybmcwKRGYBMCW' c="${1:0:1}"
+  local s="$1"; c="${s:0:1}"
+  local f=setaf out=''
   shift
 
-  [ "$c" = "-" ] && { bg=1; c="${1:1:1}"; } || { isupper $c && b=1; }
+  while [[ "$c" = [+_~-] ]]; do
+    [ "$c" = '+' ] && { out+="$(tput bold)"; }
+    [ "$c" = '-' ] && { f=setab; }
+    [ "$c" = '_' ] && { out+="$(tput smul)"; }
+    [ "$c" = '~' ] && { out+="$(tput rev)"; }
+    s="${s:1}"; c="${s:0:1}"
+  done
 
   debug $c
-  # modulo to ignore case:
   local i=$(indexof "$c" "$codes")
-  i=$(expr $i % 8)
 
-  debug $b $((i+30)) "$@"
-  _colour $b $((i+30)) "$@"
+  #debug $b $((i+30)) "$@"
+  out+="$(tput $f $i)"
+  echo -n "$out"
+  #_colour $b\;$((i+30)) "$@"
 }
 
-for _c in k r g y b m c w K R G Y B M C W -k -r -g -y -b -m -c -w; do
-  eval "@$_c(){ @ $_c \"\$@\" ; }"
+# todo: permutations of + - _ ~
+for _c in k r g y b m c w K R G Y B M C W; do
+  eval "@$_c () { @ $_c \"\$@\" ; }"
 done
 unset _c _C
 
 listcolours () {
   for ((i=0; i<=7; i++)); do
-    echo -e "\033[0;3${i}m0;3$i \033[1;3${i}m1;3$i \033[0;4${i}m0;4$i \033[0m"
+    echo -e "\033[0;3${i}m0;3$i \033[0;9${i}m0;9$i \033[1;3${i}m1;3$i \033[1;9${i}m1;9$i \033[0;4${i}m0;4$i\033[0m \033[1;4${i}m1;4$i\033[0m \033[0;10${i}m0;10$i\033[0m \033[1;10${i}m1;10$i\033[0m \033[3;3${i}m3;3${i}\033[0m \033[3;9${i}m3;9$i\033[0m \033[1;3;3${i}m1;3;3$i\033[0m \033[1;3;9${i}m1;3;9$i\033[0m \033[4;3${i}m4;3$i\033[0m \033[4;9${i}m4;9$i\033[0m \033[1;4;3${i}m1;4;3$i\033[0m"
   done
 }
 
