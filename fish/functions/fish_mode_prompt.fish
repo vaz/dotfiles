4f6ac9e212b1bc44889f3912f7b87a945fd00857
vaz <v@tryptid.com>
2016-11-25 23:50:47 -0800

    nice right git prompt

--- fish/functions/fish_mode_prompt.fish
+++ fish/functions/fish_mode_prompt.fish
@@ -4,6 +4,7 @@ function fish_mode_prompt --description 'Displays the current mode'
     set -g __fish_prompt_mode (string replace -a - _ $fish_bind_mode)
     eval set -g __fish_prompt_mode_color \$fish_color_mode_$__fish_prompt_mode
     eval set -g __fish_prompt_mode_string \$__fish_mode_string__$__fish_prompt_mode
+    set -g ___fish_git_prompt_color_branch (set_color $__fish_prompt_mode_color)
     # set_color --bold 303030 --background $__fish_prompt_mode_color
     # echo -n -s $__fish_prompt_mode_string ' '
     # set_color normal
